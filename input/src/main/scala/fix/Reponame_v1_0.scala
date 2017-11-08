626565624057b8b194802969856d230424573f3a
NOUGUIER Olivier <olivier.nouguier@gmail.com>
2017-11-08 14:43:55 +0100

    fix pure expression

--- input/src/main/scala/fix/Reponame_v1_0.scala
+++ input/src/main/scala/fix/Reponame_v1_0.scala
@@ -7,6 +7,7 @@ import cats.data.Coproduct
 
 object Reponame_v1_0_Test {
   val x = 42
+  "test"
 
   def myMethod[A, F[_], G[_]](implicit c: Coproduct[F,G,A]) = ???
 }
--- rules/src/main/scala/fix/Reponame_v1_0.scala
+++ rules/src/main/scala/fix/Reponame_v1_0.scala
@@ -2,6 +2,7 @@ package fix
 
 import scalafix._
 import scala.meta._
+import scalafix.util._
 
 final case class Reponame_v1_0(index: SemanticdbIndex)
     extends SemanticRule(index, "Reponame_v1_0") {
@@ -15,11 +16,30 @@ final case class Reponame_v1_0(index: SemanticdbIndex)
         ctx.replaceTree(t, Lit.Int(value+1).syntax)
     }.asPatch
 
+
+  private val pureExpressions = index.messages.collect {
+    case Message(pos, _, msg )
+      if(msg.startsWith("a pure expression does nothing")) => pos
+
+
+  }.toSet
+
+  def fixPureExpression(ctx: RuleCtx ) : Patch =
+    ctx.tree.collect {
+      case t: Tree if pureExpressions.contains(t.pos) =>
+        ctx.removeTokens(t.tokens) + {
+          for {
+            head <- t.tokens.headOption
+            newLine <- ctx.tokenList.leading(head).find(_.is[Newline])
+          } yield ctx.tokenList.slice(ctx.tokenList.prev(newLine), head).map(ctx.removeToken).asPatch
+        }
+    }.asPatch
+
   override def fix(ctx: RuleCtx): Patch = {
 //    ctx.tree.collect{
 //      case n @ Name("Coproduct") => println(index.symbol(n))
 //    }
-    fixLitInt(ctx) + fixCoproduc(ctx)
+    fixLitInt(ctx) + fixCoproduc(ctx) + fixPureExpression(ctx)
   }
 
 }
